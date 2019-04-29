682fe3e2e88f21890a495a38ab542bd334fbfe32
Nora Camacho <nora.camacho@outlook.com>
2019-04-29 05:38:14 -0500

    jason file engine update

--- app.js
+++ app.js
@@ -13,8 +13,11 @@ var auth = basicauth.auth;
 const app = express();
 
 //Esta instrucción permite hacer conexión en Heroku
-var port = process.env.PORT || 3000;
-app.use(bodyParser.json());
+//var port = process.env.PORT || 3000;
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
+  app.use(bodyParser.json());
 
 
 //3.- Definir paths disponibles
